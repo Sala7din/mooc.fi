ac0ab5663c2bc0171d741318d66b14b99bad86cb
Salah-Eddine Bellil <s.bellil@hotmail.de>
2020-11-23 14:04:50 +0100

    finished TelevisionPrograms

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -8,6 +8,27 @@ public class Main {
 
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        while (true){
+            System.out.print("Name: ");
+            String name = String.valueOf(scanner.nextLine());
+            if (name.isEmpty()){
+                break;
+            }
+            System.out.print("Duration: ");
+            Integer duration = Integer.valueOf(scanner.nextLine());
+            
+            programs.add(new TelevisionProgram(name, duration));
+        }
+        
+        System.out.print("Program's maximum duration? ");
+        Integer maxDuration = Integer.valueOf(scanner.nextLine());
+        for (TelevisionProgram televisionProgram: programs) {
+            if (televisionProgram.getDuration() <= maxDuration){
+                System.out.println(televisionProgram);
+            }
+            
+        }
 
     }
 }
