4dcea74c42e21c3afed3b24d85e600a9b0140423
Salah-Eddine Bellil <s.bellil@hotmail.de>
2020-11-23 16:23:00 +0100

    finished Books

---
+++ part04-Part04_20.Books/src/main/java/Book.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Salah-Eddine
+ */
+public class Book {
+    private String title;
+    private int pages;
+    private int pubYear;
+    
+    public Book(String title, int pages, int pubYear){
+        this.title = title;
+        this.pages = pages;
+        this.pubYear = pubYear;
+    }
+    
+    public String getTitle() {
+        return this.title;
+    }
+    
+    public int getPages() {
+        return this.pages;
+    }
+    
+    public int getPubYear(){
+        return this.pubYear;
+    }
+    
+    public String toString() {
+        return this.title + ", " + this.pages + " pages, " + this.pubYear;
+    }
+    
+}
--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -7,6 +7,39 @@ public class Main {
         
         // implement here the program that allows the user to enter 
         // book information and to examine them
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> books = new ArrayList<>();
+        
+        while (true){
+            System.out.print("Title: ");
+            String title = String.valueOf(scanner.nextLine());
+            if (title.isEmpty()){
+                break;
+            }
+            System.out.print("Pages: ");
+            int pages = Integer.valueOf(scanner.nextLine());
+            
+            System.out.println("Publication year: ");
+            int pubYear = Integer.valueOf(scanner.nextLine());
+            
+            books.add(new Book(title, pages, pubYear));
+            
+        }
+        System.out.print("What information will be printed? ");
+        String information = String.valueOf(scanner.nextLine());
+        
+        for (Book book: books){
+            if (information.equals("everything")){
+                System.out.println(book);
+            }
+            if (information.equals("name")){
+                System.out.println(book.getTitle());
+                
+            
+            }
+            
+        }
+       
 
     }
 }
