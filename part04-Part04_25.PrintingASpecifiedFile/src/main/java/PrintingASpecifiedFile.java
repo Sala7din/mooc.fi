2cbef9380c4857bd2aa4d763cf9b772cd823a311
Salah-Eddine Bellil <s.bellil@hotmail.de>
2020-11-26 20:12:32 +0100

    finished PrintingASpecifedFile

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,21 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Which file should have its contents printed?");
+        String fileName = String.valueOf(scanner.nextLine());
+        
+        try (Scanner scanner1 = new Scanner(Paths.get(fileName))) {
+            
+            while (scanner1.hasNextLine()){
+                
+                String row = scanner1.nextLine();
+                
+                System.out.println(row);
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 }
