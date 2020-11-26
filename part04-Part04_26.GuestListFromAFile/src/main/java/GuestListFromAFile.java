39a9fef8ecafc4607a1fd5bd94be9274c4e80a90
Salah-Eddine Bellil <s.bellil@hotmail.de>
2020-11-26 21:00:30 +0100

    finished GuestListFromAFile

--- part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
+++ part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
@@ -1,18 +1,25 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GuestListFromAFile {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Name of the file:");
         String file = scanner.nextLine();
 
         ArrayList<String> list = new ArrayList<>();
-        // implement reading the file here.
+        
+        try (Scanner scanner1 = new Scanner(Paths.get(file))) {
+            
+            while (scanner1.hasNextLine()) {
+                list.add(scanner1.nextLine());
+            }
+        }
         System.out.println("");
 
         System.out.println("Enter names, an empty line quits.");
